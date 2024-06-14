fec554d04de31d172b47e76de3fdf9787e970b60
shivam bole <shivamofficial285@gmail.com>
2024-06-14 23:42:08 +0530

    make pages responsive and add contact page

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     images: {
       domains: ['www.xoriant.com'],
     },
+    images: {
+      domains: ['dynamicmedia.accenture.com'],
+    }
+    
 };
 
 export default nextConfig;
--- src/app/components/WorkCard/WorkCard.tsx
+++ src/app/components/WorkCard/WorkCard.tsx
@@ -2,21 +2,23 @@ import React from 'react'
 import Image from 'next/image'
 import './WorkCard.css'
 
-interface WorkBalanceProps {
-   imgages:String,
-   altText: string,
-   title:String,
-   text:String
-  }
-const WorkCard: React.FC<WorkBalanceProps>  = ({ imgages, title, text, altText }) => {
-  return (
-    <div className='card'>
-      <Image src={`${imgages}`} alt={altText}  className='cardImag'/>
-   
-    <h3>{title}</h3>
-    <h4>{text}</h4>
-    </div>
-  )
+
+const WorkCard = ({ images, altText, text, title }) => {
+    return (
+        <div className='work-card  lg:flex justify-around shadow-md rounded-xl w-[100%] mb-5 p-4'>
+            <Image 
+                src={images} 
+                alt={altText} 
+                width={480} // specify the width
+                height={400} // specify the height
+                className='work-card-image rounded-lg border-0' 
+            />
+            <div className='work-card-content lg:w-[40%] w-[100%] mt-5'>
+            <p className="text-black lg:text-5xl text-3xl text-center work-card-title pt-6 font-bold ">{title}</p>
+                <p className='work-card-text'>{text}</p>
+            </div>
+        </div>
+    )
 }
 
 export default WorkCard
