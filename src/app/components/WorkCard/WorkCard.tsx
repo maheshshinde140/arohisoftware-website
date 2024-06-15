e3fa66996324f2e7c64dedcb84a487048a64a1bc
shivam bole <shivamofficial285@gmail.com>
2024-06-15 17:47:45 +0530

    change job page theme

--- src/app/components/WorkCard/WorkCard.tsx
+++ src/app/components/WorkCard/WorkCard.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import Image from 'next/image';
-import './WorkCard.css';
-
+import React from "react";
+import Image from "next/image";
+import "animate.css";
+import "./WorkCard.css";
 
 const WorkCard = ({ images, altText, text, title }) => {
-    return (
-        <div className='work-card  lg:flex justify-around shadow-md rounded-xl w-[100%] mb-5 p-4'>
-            <Image 
-                src={images} 
-                alt={altText} 
-                width={480} // specify the width
-                height={400} // specify the height
-                className='work-card-image rounded-lg border-0' 
-            />
-            <div className='work-card-content lg:w-[40%] w-[100%] mt-5'>
-            <p className="text-black lg:text-5xl text-3xl text-center work-card-title pt-6 font-bold ">{title}</p>
-                <p className='work-card-text'>{text}</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundImage: `url(${images})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="card-content">
+        <h2 className="card-title my-5">{title}</h2>
+        <p className="card-body my-5">{text}</p>
+        <a href="#" className="button">
+          Learn More
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default WorkCard
+export default WorkCard;
--- src/app/experienceProf/page.tsx
+++ src/app/experienceProf/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import WorkCard from '@/app/components/WorkCard/WorkCard'
 import Footer from '@/app/components/Footer'
+import StayConnected from '../components/StayConnected';
 
 const page = () => {
     return (
-        <div>
-            <div className='bg-white flex flex-col-reverse lg:flex-row justify-center items-center text-black h-[100vh]'>
+        <div className='bg-white'>
+            <div className=' flex flex-col-reverse lg:flex-row justify-center items-center text-black h-[100vh]'>
                 <div className='text-start md:ml-24 p-5 md:w-[80vw]'>
                     <h1 className='text-black font-bold text-3xl md:text-6xl mb-5'>Careers for <br /> Experienced Professionals</h1>
                     <p className='text-black font-medium mb-5'>Bring your expertise to some of the most influential organizations in the world, transforming industries alongside the best of the best.</p>
@@ -18,13 +19,13 @@ const page = () => {
                 }}></div>
             </div>
 
-            <div className='bg-gray-200 border text-black'>
-                <div className='container lg:w-[80vw] my-5  m-auto'>
-                    <h1 className='text-black font-bold text-3xl my-5 lg:text-6xl mb-5'>Chart your course</h1>
-                    <p className='mb-5'>Get to know Accenture and find your fit. If you want to contribute across a range of industries and make a real difference, here’s where your experience could be put to work:</p>
-                    <div className='flex justify-between space-y-5 flex-wrap'>
+            <div className='bg-gray-200 text-black'>
+                <div className='container my-5 pt-8 m-auto'>
+                    <h1 className='text-black font-bold text-3xl lg:text-5xl'>Chart your course</h1>
+                    <p className='mb-5 mt-5 lg:max-w-[60%]'>Get to know Arohi Softwares and find your fit. If you want to contribute across a range of industries and make a real difference, here’s where your experience could be put to work:</p>
+                    <div className='grid lg:grid-cols-3 gap-5'>
                         <WorkCard 
-                            images='https://dynamicmedia.accenture.com/is/image/accenture/Accenture-work-environment1-768x432?qlt=85&wid=480&ts=1704397328444&fit=constrain&dpr=off' 
+                            images='https://dynamicmedia.accenture.com/is/image/accenture/Group-Discussions:16x9?ts=1710823877471&fit=constrain&dpr=off&wid=480' 
                             altText='this is img' 
                             text='We provide several voluntary options, including job-sharing opportunities, part-time and flex-time schedules.' 
                             title="Flexible Schedules" 
@@ -62,6 +63,7 @@ const page = () => {
                     </div>
                 </div>
             </div>
+            <StayConnected/>
             <Footer />
         </div>
     );
