0dfb7cbb9ca8d7b5b8af48aafccc96da9563e67b
shivam bole <shivamofficial285@gmail.com>
2024-06-20 19:52:25 +0530

    make change in home page

--- src/app/components/HomePageComponent.tsx
+++ src/app/components/HomePageComponent.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const HomePageComponent: React.FC = () => {
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-semibold  text-center mb-6">
-        Every day, we embrace change and create value for all our <br />
-        stakeholders, in every part of the world.
+      <h1 className="text-3xl font-semibold  text-center mb-6 lg:w-[66vw] m-auto">
+    
+Every single day, we transform and add value for our stakeholders everywhere. 
       </h1>
-
-      <div className="flex flex-wrap lg:justify-around lg:my-20 ">
+<h1 className="text-3xl font-semibold  text-center mb-6"></h1>
+      <div className="flex flex-wrap lg:justify-evenly lg:my-20 mx-">
 
         <div>
         <img 
@@ -18,7 +18,7 @@ const HomePageComponent: React.FC = () => {
         />
         </div>
 
-        <div className=" lg:w-[33%] w-96 my-10 lg:mt-36 lg:ml-72">
+        <div className=" lg:w-[33%] w-96 my-10 lg:mt-36 lg:ml-40">
           <h2 className="text-xl  font-semibold">Arohi Software Elevating Businesses with Expert IT Solutions</h2>
           <p className="mt-2">
           Arohi Software specializes in transformative IT solutions, offering bespoke web development, mobile app creation, and enterprise software services. Our innovative approach and commitment to excellence ensure tailored solutions that enhance efficiency and drive growth for businesses of all sizes. Partner with us for unparalleled digital success.
