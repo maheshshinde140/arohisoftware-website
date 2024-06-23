7422ebe68e1ba311e607218a9495157e98fe814a
Mahesh Shinde <97420827+maheshshinde140@users.noreply.github.com>
2024-06-23 15:50:05 +0530

    deploy

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -35,7 +35,7 @@ export async function POST(request: NextRequest){
             email: user.email
         }
         //create token
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: "1d"})
+        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" })
 
         const response = NextResponse.json({
             message: "Login successful",
--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,24 +1,27 @@
 'use client';
-// pages/location.tsx
 import { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import GoogleMapReact from 'google-map-react';
 import Footer from '../components/Footer';
 import Head from 'next/head';
 import { ArrowRightIcon } from '@radix-ui/react-icons';
 
-const markerIcon = new L.Icon({
-  iconUrl: 'https://www.pikpng.com/pngl/b/66-660381_business-icon-company-name-icon-clipart.png', // Use the correct URL for your marker icon
-  iconSize: [38, 38], // Increased the size of the marker icon
-  iconAnchor: [19, 38], // Adjusted the anchor point
-});
+interface AnyReactComponentProps {
+  lat: number;
+  lng: number;
+  text: string;
+}
+
+const AnyReactComponent: React.FC<AnyReactComponentProps> = ({ lat, lng, text }) => (
+  <div>
+    <div>{text}</div>
+  </div>
+);
 
 const LocationPage = () => {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   const [location, setLocation] = useState({
-    x: 19.9278796,
-    y: 74.73809
+    lat: 19.9278796,
+    lng: 74.73809
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -31,6 +34,10 @@ const LocationPage = () => {
     console.log(formData);
   };
 
+  const handleLocationChange = (lat: number, lng: number) => {
+    setLocation({ lat, lng });
+  };
+
   return (
     <div className=" mt-12 flex flex-col items-center justify-center p-4">
       <Head>
@@ -39,29 +46,26 @@ const LocationPage = () => {
       </Head>
 
       <div className="relative w-full">
-      <div className="absolute left-1/2 transform -translate-x-1/2 top-1/4 bg-white/60 backdrop-blur-lg text-center text-black py-8 px-4 z-10 w-3/4 md:w-1/2 rounded-lg shadow-lg">
-  <h1 className="text-3xl md:text-4xl font-semibold mb-4">Arohi Softwares IT Company</h1>
-  <p className="text-base md:text-lg px-6 ">Find us at our office or get in touch with us for any inquiries. <span className="text-base md:text-lg px-6 hidden md:block">We are here to help you!</span></p>
-</div>
-
-
-
+        <div className="absolute left-1/2 transform -translate-x-1/2 top-1/4 bg-white/60 backdrop-blur-lg text-center text-black py-8 px-4 z-10 w-3/4 md:w-1/2 rounded-lg shadow-lg">
+          <h1 className="text-3xl md:text-4xl font-semibold mb-4">Arohi Softwares IT Company</h1>
+          <p className="text-base md:text-lg px-6">Find us at our office or get in touch with us for any inquiries. <span className="text-base md:text-lg px-6 hidden md:block">We are here to help you!</span></p>
+        </div>
         <img className="w-full h-96 object-cover -z-10" src="https://cdn.dribbble.com/users/2096861/screenshots/18532623/media/7567479d6dd3db1282da37267a9cacc6.jpg" alt="Background" />
       </div>
 
       <main className="flex flex-col items-center justify-center w-full h-full p-6 bg-black shadow-lg rounded-lg z-10">
         <section className="flex flex-col md:flex-row justify-center gap-6 md:gap-12 w-full mb-12">
           <div className="hover:bg-gradient-to-br p-[2px] from-violet-400 to-orange-300 w-full md:w-1/2">
-            <button className="group w-full text-left border rounded-md flex justify-between items-center p-5 bg-white text-black" onClick={() => setLocation({ x: 19.0945, y: 74.7381 })}>
+            <button className="group w-full text-left border rounded-md flex justify-between items-center p-5 bg-white text-black" onClick={() => handleLocationChange(19.0945, 74.7381)}>
               <div>
-                <h3 className="text-2xl">Arohi Software Development,near Sai hospital building,bhairavnath chawk ,Shrigoanda, Maharashtra</h3>
+                <h3 className="text-2xl">Arohi Software Development, near Sai hospital building, bhairavnath chawk, Shrigoanda, Maharashtra</h3>
                 <p>Full Address</p>
               </div>
               <ArrowRightIcon className="scale-150 transition-all -translate-x-6 group-hover:-translate-x-2" />
             </button>
           </div>
           <div className="hover:bg-gradient-to-br p-[2px] from-violet-400 to-orange-300 w-full md:w-1/2">
-            <button className="group w-full text-left border p-5 rounded-md flex justify-between items-center bg-white text-black" onClick={() => setLocation({ x: 18.5204, y: 73.8567 })}>
+            <button className="group w-full text-left border p-5 rounded-md flex justify-between items-center bg-white text-black" onClick={() => handleLocationChange(18.5204, 73.8567)}>
               <div>
                 <h3 className="text-3xl">Pune, Maharashtra</h3>
                 <p>Upcoming</p>
@@ -71,16 +75,19 @@ const LocationPage = () => {
           </div>
         </section>
         <div className="border-8 w-full md:w-[70vw] h-96 md:h-[70vh] rounded-lg overflow-hidden shadow-md">
-          <MapContainer center={[location.x, location.y]} zoom={13} style={{ height: '100%', width: '100%' }}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[location.x, location.y]} icon={markerIcon} >
-              <Popup>
-                Arohi Softwares Location
-              </Popup>
-            </Marker>
-          </MapContainer>
+          <div style={{ height: '100%', width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: "" }} // Add your Google Maps API key here
+              center={location}
+              zoom={11}
+            >
+              <AnyReactComponent
+                lat={location.lat}
+                lng={location.lng}
+                text="Arohi Software"
+              />
+            </GoogleMapReact>
+          </div>
         </div>
       </main>
 
@@ -127,7 +134,7 @@ const LocationPage = () => {
           <p className="text-blue-900">or contact us via phone: <a href="tel:+1234567890" className="text-blue-900">+1 (234) 567-890</a></p>
         </div>
       </form>
-    <Footer/>
+      <Footer/>
     </div>
   );
 };
